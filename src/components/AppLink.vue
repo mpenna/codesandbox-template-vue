a3dc318964809e937c49c0eca1b13abea387d386
Marcos Penna <mpenna@users.noreply.github.com>
2023-05-26 22:06:18 +0000

    flex

--- src/components/AppLink.vue
+++ src/components/AppLink.vue
@@ -1,36 +1,48 @@
 <script setup>
-import { RouterLink, useLink } from 'vue-router'
-import { computed } from 'vue'
+import { computed, toRefs } from 'vue'
+import { RouterLink } from 'vue-router'
 
 const props = defineProps({
   ...RouterLink.props,
-  // activeClass: String,
-  // activeExactClass: String,
-  // inactiveClass: String,
+
+  activeClass: {
+    type: String,
+    default: '',
+  },
+
+  inactiveClass: {
+    type: String,
+    default: '',
+  },
 })
 
-const {
-  // the resolved route object
-  route,
-  // the href to use in a link
-  href,
-  // boolean ref  indicating if the link is active
-  isActive,
-  // boolean ref  indicating if the link is exactly active
-  isExactActive,
-  // function to navigate to the link
-  navigate
-} = useLink(props)
+const { to, activeClass, inactiveClass } = toRefs(props)
 </script>
 
 <template>
+  <router-link v-slot="{ isActive, href, navigate }" v-bind="$props" custom>
+    <a v-bind="$attrs" :href="href" :class="isActive ? activeClass : inactiveClass" @click="navigate">
+      <slot />
+    </a>
+  </router-link>
+</template>
+<!-- 
+<template>
   <router-link v-bind="$props" custom v-slot="{ href, route, navigate, isActive, isExactActive }">
-    <!-- <li :class="[isActive && 'router-link-active', isExactActive && 'router-link-exact-active']"> -->
     <li :class="[isActive && activeClass, isExactActive && activeExactClass]">
-      <!-- <a :href="href" @click="navigate">{{ route.fullPath }}</a> -->
       <a :href="href" @click="navigate">
         <slot />
       </a>
     </li>
   </router-link>
-</template>
+</template> -->
+
+<!-- <template>
+  <router-link v-bind="$props" custom v-slot="{ href, route, navigate, isActive, isExactActive }">
+    <li :class="[isActive && 'router-link-active', isExactActive && 'router-link-exact-active']">
+      <a :href="href" @click="navigate">{{ route.fullPath }}
+        <slot />
+      </a>
+    </li>
+  </router-link>
+</template> -->
--- src/components/NavLink.vue
+++ src/components/NavLink.vue
@@ -6,6 +6,14 @@
     exact-active-class="flex items-center gap-x-3 py-2 px-2.5 bg-gray-700 text-sm text-white rounded-md">
     <slot />
   </AppLink>
+  
+  <!-- <AppLink v-bind="$attrs"
+    class="flex items-center gap-x-3.5 py-2 px-2.5 text-sm text-gray-400 rounded-md hover:bg-gray-800 hover:text-white-300"
+    active-class="flex items-center gap-x-3 py-2 px-2.5 bg-gray-700 text-sm text-white rounded-md"
+    exact-active-class="flex items-center gap-x-3 py-2 px-2.5 bg-gray-700 text-sm text-white rounded-md">
+    <slot />
+  </AppLink>
+ -->
   <!-- <AppLink v-bind="$attrs"
     class="inline-flex items-center px-1 pt-1 border-b-2 border-transparent text-sm font-medium leading-5 text-gray-500 hover:text-gray-700 hover:border-gray-300 focus:outline-none focus:text-gray-700 focus:border-gray-300 transition duration-150 ease-in-out"
     active-class="border-indigo-500 text-gray-900 focus:border-indigo-700"
