85c7f97b626e80718e6ce79d88ca2f41f79f5b7a
Marcos Penna <mpenna@users.noreply.github.com>
2023-05-26 22:24:01 +0000

    flex

--- src/components/AppLink.vue
+++ src/components/AppLink.vue
@@ -10,22 +10,38 @@ const props = defineProps({
     default: '',
   },
 
+  exactActiveClass: {
+    type: String,
+    default: '',
+  },
+
   inactiveClass: {
     type: String,
     default: '',
   },
 })
 
-const { to, activeClass, inactiveClass } = toRefs(props)
+const { /* to, */ activeClass, exactActiveClass, inactiveClass } = toRefs(props)
 </script>
 
+<template>
+  <router-link v-slot="{ isActive, isExactActive, href, navigate }" v-bind="$props" custom>
+    <a v-bind="$attrs" :href="href" :class="isActive ? activeClass : (isExactActive ? exactActiveClass : inactiveClass)"
+      @click="navigate">
+      <slot />
+    </a>
+  </router-link>
+</template>
+
+<!-- 
 <template>
   <router-link v-slot="{ isActive, href, navigate }" v-bind="$props" custom>
     <a v-bind="$attrs" :href="href" :class="isActive ? activeClass : inactiveClass" @click="navigate">
       <slot />
     </a>
   </router-link>
-</template>
+</template> -->
+
 <!-- 
 <template>
   <router-link v-bind="$props" custom v-slot="{ href, route, navigate, isActive, isExactActive }">
